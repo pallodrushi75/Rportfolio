f2b04a0e0b07527f182a9fd691d0289c3f3bfeac
pallodrushi75 <pallodrushikesh75@gmail.com>
2022-07-24 00:42:59 +0530

    Case Study Updated

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import lottie from "lottie-web";
 import HomeBio from "../assets/img/Home-bio.png";
 import { render } from "react-dom";
-import Proj1 from "../assets/img/proj1.png";
+import Case1 from "../assets/img/case1.png";
+import Case2 from "../assets/img/case2.png";
+import Case3 from "../assets/img/case3.webp";
 import Proj2 from "../assets/img/proj2.png";
 import Proj3 from "../assets/img/proj3.png";
 import Footer from "../components/Footer";
@@ -19,7 +21,7 @@ const CaseStudy = () => {
             <img
               data-aos="flip-left"
               data-aos-duration="1000"
-              src={Proj1}
+              src={Case1}
               alt="Banking System"
               className="pr-image"
             />
@@ -30,30 +32,20 @@ const CaseStudy = () => {
             className="col-12 col-md-6"
           >
             <div>
-              <p className="h1 title">Basic Banking System</p>
+              <p className="h1 title">Product Improvement - Bumble</p>
             </div>
             <div>
               <p className="justify-text-res lead">
-                This is Basic Banking system where one can transact money and
-                transactions will be reflected in transaction history modal
-                along with the time of transfer.
+              Designed Solutions to increase Date to Match ratio of the dating app.
+Starting from user personas to finding solutions on the painpoints and then prioritizing features, it
+was a team project.
+Wireframing and deciding metrics was also a part of this project at the DoremonDen PM cohort
               </p>
             </div>
-            <div className="icons">
-              <div
-                className="iconify"
-                data-icon="vscode-icons:file-type-html"
-              ></div>
-              <div
-                className="iconify"
-                data-icon="vscode-icons:file-type-css"
-              ></div>
-              <div className="iconify" data-icon="logos:bootstrap"></div>
-              <div className="iconify" data-icon="logos:javascript"></div>
-            </div>
+
             <div className="mt-8">
               <a
-                href="https://pallodrushi75.github.io/WebDev_Task1/index.html"
+                href="https://drive.google.com/file/d/1HEFXx_dH02AGbChYT6IS2zT7aC-Uldsg/view"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -73,29 +65,21 @@ const CaseStudy = () => {
             className="col-12 col-md-6"
           >
             <div>
-              <p className="h1 title">Github Search</p>
+              <p className="h1 title">Discord Innovative Features</p>
             </div>
             <div>
               <p className="justify-text-res lead">
-                This is a simple site where one can enter anyone's gtihub username and get data of all the public repos of that user.
-                
+              Select any B2C software product (app/web app) that you like the
+most. Highlight the three most interesting and innovative features of
+that product/platform. Indicate why you think it is innovative and
+what value it offers to the end consumer and the business. Use your
+style of presentation to complete your answers.
               </p>
             </div>
-            <div className="icons">
-              <div
-                className="iconify"
-                data-icon="vscode-icons:file-type-html"
-              ></div>
-              <div
-                className="iconify"
-                data-icon="vscode-icons:file-type-css"
-              ></div>
-              <div className="iconify" data-icon="logos:bootstrap"></div>
-              <div className="iconify" data-icon="vscode-icons:file-type-reactjs"></div>
-            </div>
+
             <div className="mt-8">
               <a
-                href="https://reactgraphql.netlify.app/"
+                href="https://drive.google.com/file/d/1mMl7fFKXhdTGhIW1m8p2tDeCLg3W-6TK/view?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -109,7 +93,7 @@ const CaseStudy = () => {
             <img
               data-aos="flip-left"
               data-aos-duration="1000"
-              src={Proj2}
+              src={Case2}
               alt="Banking System"
               className="pr-image"
             />
@@ -122,7 +106,7 @@ const CaseStudy = () => {
             <img
               data-aos="flip-left"
               data-aos-duration="1000"
-              src={Proj3}
+              src={Case3}
               alt="Banking System"
               className="pr-image"
             />
@@ -133,27 +117,33 @@ const CaseStudy = () => {
             className="col-12 col-md-6"
           >
             <div>
-              <p className="h1 title">Hand Gesture Controlled Media</p>
+              <p className="h1 title">News Aggregator Platform </p>
             </div>
             <div>
               <p className="justify-text-res lead">
-                A hardware project using ultrasonic sensors and arduino UNO to detect hand movements and using the detected data to control certain functions of VLC media player such as play/pause, forward/rewind and volume increase/decrease with the help of python and pyautogui.
-              </p>
+              There are multiple platforms providing different types of news.
+Users have to hop through different platforms for different types of
+news. There is a need for an intuitive new aggregator platform
+where the user can decide which types of news they want from
+which website/source.
+                </p>
             </div>
             <div className="icons">
-              <div
-                className="iconify"
-                data-icon="logos:python"
-              ></div>
-              <div
-                className="iconify"
-                data-icon="vscode-icons:file-type-arduino"
-              ></div>
+
 
             </div>
             <div className="mt-8">
-
+              <a
+                href="https://drive.google.com/file/d/19TFsrIBXKOXq2XjxWMBhIfgwaCMril-t/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="pr-btn">
+                  Link
+                </button>
+              </a>
             </div>
+          
           </div>
         </div>
 
